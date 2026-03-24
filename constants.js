ef5a9378f156cdb4a94f66ff42b0daae744851ae
Tereza Khachatryan <Tereza.Khachatryan@hearst.com>
2026-03-24 13:08:55 +0400

    first commit/added cards

---
+++ constants.js
@@ -0,0 +1,20 @@
+export const cardDetails = [
+    {
+        id: 1,
+        name: "SEDAN",
+        decs: "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+        img: "./assets/SEDAN.svg",
+    },
+    {
+        id: 2,
+        name: "SUVS",
+        decs: "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+        img: "./assets/SUVS.svg"
+    },
+    {
+        id: 3,
+        name: "LUXURY",
+        decs: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+        img: "./assets/LUXURY.svg"
+    }
+]
---
+++ index.js
@@ -0,0 +1,20 @@
+import { cardDetails } from "./constants.js";
+
+const cards = document.querySelector(".cards");
+function createCard() {
+    cardDetails.forEach((card) => {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.innerHTML = `
+            <img src=${card.img} alt=${card.name} class="card-img">
+            <h1 class="card-title">${card.name}</h1>
+            <p class="card-desc">${card.decs}</p>
+        `
+        const button = document.createElement("button");
+        button.classList.add("card-btn");
+        button.textContent = "Learn More";
+        cardElement.appendChild(button);
+        cards.appendChild(cardElement);
+    })
+}
+createCard()
